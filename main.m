6ed9acaebd1743fbfe7919a74e78ca18bdfc81ae
Tobias Seidler <tseidler@student.ethz.ch>
2018-12-03 10:02:22 +0100

    no message

--- main.m
+++ main.m
@@ -5,10 +5,10 @@
 %   /_______/  /__/  /__/  /__/
 %   Swiss Federal Institute of Technology Zurich
 %
-%    Process Simulation and Flowsheeting HS18
-%   Semester Task Solver Code
+%   Process Simulation and Flowsheeting HS18
+%   Semester Task Solver Code for the Williams-Otto-Process
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%   Code written by Tobias Seidler on 27/11/2018 - V1.0   %
+%   Code written by Tobias Seidler on 12/12/2018 - V1.0   %
 %   Tested on MATLAB R2017b, MBPro Build A1398, osX 10.14 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
