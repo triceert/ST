4efad0b4d31511ec5012b003dce37c8e115733d5
Tobias Seidler <tseidler@student.ethz.ch>
2018-12-03 09:47:01 +0100

    initial main file

---
+++ main.m
@@ -0,0 +1,21 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%      ____________________   ___                          %
+%     /  ________   ___   /__/  /
+%    /  _____/  /  /  /  ___   /
+%   /_______/  /__/  /__/  /__/
+%   Swiss Federal Institute of Technology Zurich
+%
+%    Process Simulation and Flowsheeting HS18
+%   Semester Task Solver Code
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Code written by Tobias Seidler on 27/11/2018 - V1.0   %
+%   Tested on MATLAB R2017b, MBPro Build A1398, osX 10.14 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function main
+
+
+
+
+end
+
